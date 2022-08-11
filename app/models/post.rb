1c311ae065047e479e61ed82bde6577afacd1235
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-08-11 12:30:58 +0800

    Add validation for post model that comments_counter must be an integer greater than or equal to zero

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,4 +12,5 @@ class Post < ApplicationRecord
   end
 
   validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
