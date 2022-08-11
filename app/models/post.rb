abe364e6731fcbeb6fa1de336bf40981fe061559
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-08-11 10:27:18 +0800

    Create a custom method that returns the 5 most recent comments for a given post

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,11 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
-  def update_post_counter
+  def update_posts_counter
     user.update(posts_counter: user.posts.count)
+  end
+
+  def recent_5_comments
+    comments.limit(5).order(created_at: :desc)
   end
 end
