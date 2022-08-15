aa83fc6498b286642414865df55e813769b0aa54
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-08-15 16:57:57 +0800

    Generate Users controller, route and view files

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/:id', to: 'users#show', as: 'user'
+  get 'users', to: 'users#index', as: 'users'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
