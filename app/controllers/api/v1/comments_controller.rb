dd43defffb44b9cf6c34dba9bfa51dabd1c45fc9
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-08-26 16:03:24 +0500

    Create comments controller to render json

---
+++ app/controllers/api/v1/comments_controller.rb
@@ -0,0 +1,33 @@
+class Api::V1::CommentsController < ApplicationController
+  def index
+    @user = User.find(params[:user_id])
+    @posts = @user.posts.find(params[:post_id])
+    @comments = @posts.comments
+
+    respond_to do |format|
+      format.json { render json: @comments }
+    end
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @post = @user.posts.find(params[:post_id])
+    @comment = @post.comments.create(text: comment_parameters[:text], author_id: @user.id, post_id: @post.id)
+
+    if @comment.save
+      respond_to do |format|
+        format.json { render json: @comment, status: :created }
+      end
+    else
+      respond_to do |format|
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def comment_parameters
+    params.require(:comment).permit(:text)
+  end
+end
